e35285fa83a991631a275ddfc089d93799d179bf
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-11-29 20:33:07 +0400

    Move translation.schema to project root to enable easier $schema syntax

--- packages/intl-schematic/generate-schema.js
+++ packages/intl-schematic/generate-schema.js
@@ -2,6 +2,6 @@ import { writeFile } from 'fs/promises';
 
 import { compileFromFile } from 'json-schema-to-typescript';
 
-const schema = await compileFromFile('./src/translation.schema.json');
+const schema = await compileFromFile('./translation.schema.json');
 
 await writeFile('./src/translation.schema.d.ts', schema);
