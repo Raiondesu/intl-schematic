16a01d57d979cbba5707a7e86eb5b251b78d8af0
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-12-20 09:11:26 +0400

    Feature tsup.config: automatic dependency externalizer

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 import { readdir, readFile, writeFile } from 'fs/promises';
 import { existsSync } from 'fs';
-import { blue, bold } from 'colorette';
+import { cyan, bold, yellow, green } from 'colorette';
 import { fdir } from 'fdir';
 import { join } from 'path/posix';
 
@@ -20,60 +20,78 @@ const entries = new fdir()
   .sync()
   .map(p => join(p, 'src'));
 
-const CLI = blue('CLI');
-const pathsLog = `${CLI} 📦 ${bold('Export paths')}: {`;
+const CFG = cyan('CFG');
 
-export default defineConfig(entries.map(entry => ({
-  entry: [entry],
-  outDir: entry.replace('src', 'dist'),
-  format: ['cjs', 'esm'],
-  metafile: true,
-  splitting: false,
-  platform: 'neutral',
-  tsconfig: entry.replace('src', 'tsconfig.json'),
-  external: [/intl-schematic/, 'solid-js', 'rambda'],
-  clean: true,
+export default defineConfig(() => Promise.all(entries.map(async entry => {
+  const cwd = (p: string) => join(process.cwd(), entry.replace('src', ''), p);
 
-  async onSuccess() {
-    const cwd = (p: string) => join(process.cwd(), entry.replace('src', ''), p);
+  const packageInfo = JSON.parse((await readFile(cwd('./package.json'))).toString());
 
-    const outputs = (await readdir(cwd('./dist'), { recursive: true }))
-      .map(p => p.replace('\\', '/'))
-      .filter(o => /\.js$/.test(o));
+  const dependencies = [
+    ...Object.keys(packageInfo.dependencies ?? {}),
+    ...Object.keys(packageInfo.peerDependencies ?? {}),
+    ...Object.keys(packageInfo.devDependencies ?? {}),
+    ...Object.keys(packageInfo.optionalDependencies ?? {}),
+  ];
 
-    const packageInfo = JSON.parse((await readFile(cwd('./package.json'))).toString());
+  const dependenciesLogs = '[\n'.concat(
+    ...dependencies.map((d, i, arr) => green(`${CFG}\t'${d}'${i === arr.length - 1 ? '' : ','}\n`)),
+    `${CFG} ]`
+  );
 
-    const exportPaths = outputs.reduce((obj, path) => ({
-      ...(path.startsWith('index') ? { ['.']: {
-        import: './dist/' + path,
-        require: './dist/' + path.replace('.js', '.cjs'),
-        types: './src/' + path.replace('.js', '.ts')
-      } } : {}),
-      ...obj,
-      ['./' + path.replace('.js', '')]: {
-        import: './dist/' + path,
-        require: './dist/' + path.replace('.js', '.cjs'),
-        types: './src/' + path.replace('.js', '.ts')
-      }
-    }), {});
+  console.log(`${CFG} 📥 ${yellow(packageInfo.name)} ${bold('dependencies')} to be marked as external:`, dependenciesLogs);
 
-    console.log(pathsLog);
-    JSON.stringify(exportPaths, null, 2)
-      .split('\n')
-      .slice(1)
-      .map(line => console.log(`${CLI} ${line}`));
+  const pathsLog = `${CFG} 📦 ${bold('Export paths')} of ${yellow(packageInfo.name)}: {`;
 
-    packageInfo.exports = exportPaths;
-    packageInfo.typesVersions = {
-      '*': Object.keys(exportPaths).reduce((obj, p) => ({
+  return ({
+    entry: [entry],
+    outDir: entry.replace('src', 'dist'),
+    format: ['cjs' as const, 'esm' as const],
+    metafile: true,
+    splitting: false,
+    platform: 'neutral' as const,
+    tsconfig: entry.replace('src', 'tsconfig.json'),
+    external: dependencies,
+    clean: true,
+
+    async onSuccess() {
+
+      const outputs = (await readdir(cwd('./dist'), { recursive: true }))
+        .map(p => p.replace('\\', '/'))
+        .filter(o => /\.js$/.test(o));
+
+      const exportPaths = outputs.reduce((obj, path) => ({
+        ...(path.startsWith('index') ? { ['.']: {
+          import: './dist/' + path,
+          require: './dist/' + path.replace('.js', '.cjs'),
+          types: './src/' + path.replace('.js', '.ts')
+        } } : {}),
         ...obj,
-        [p.replace('./', '')]: [exportPaths[p].types],
-      }), {}),
-    };
+        ['./' + path.replace('.js', '')]: {
+          import: './dist/' + path,
+          require: './dist/' + path.replace('.js', '.cjs'),
+          types: './src/' + path.replace('.js', '.ts')
+        }
+      }), {});
 
-    await writeFile(cwd('./package.json'), JSON.stringify(packageInfo, null, 2));
-  },
-  // banner: {
-  //   js: `/*\n${readFileSync('./LICENSE').toString('utf-8')}*/`
-  // }
+      console.log(pathsLog);
+      JSON.stringify(exportPaths, null, 2)
+        .split('\n')
+        .slice(1)
+        .map(line => console.log(`${CFG} ${line}`));
+
+      packageInfo.exports = exportPaths;
+      packageInfo.typesVersions = {
+        '*': Object.keys(exportPaths).reduce((obj, p) => ({
+          ...obj,
+          [p.replace('./', '')]: [exportPaths[p].types],
+        }), {}),
+      };
+
+      await writeFile(cwd('./package.json'), JSON.stringify(packageInfo, null, 2));
+    },
+    // banner: {
+    //   js: `/*\n${readFileSync('./LICENSE').toString('utf-8')}*/`
+    // }
+  });
 })));
