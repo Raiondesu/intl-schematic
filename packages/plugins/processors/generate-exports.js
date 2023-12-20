e073569dddd806f0318a8ecf964ddddf9a15430e
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-12-20 13:59:22 +0400

    Remove unused script

--- packages/plugins/processors/generate-exports.js
+++
@@ -1,8 +0,0 @@
-import { readdirSync } from 'fs';
-
-const paths = readdirSync('./src', { recursive: true })
-  .map(p => p.replace('\\', '/'));
-
-const normalExports = paths.reduce((obj, path) => {
-  return obj;
-}, {});
