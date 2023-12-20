ec4d0d14078d980fe1a3a394eb6138bd1a2ce16d
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-12-20 13:57:34 +0400

    Remove json-schema-to-typescript

--- packages/core/generate-schema.js
+++
@@ -1,7 +0,0 @@
-import { writeFile } from 'fs/promises';
-
-import { compileFromFile } from 'json-schema-to-typescript';
-
-const schema = await compileFromFile('./translation.schema.json');
-
-await writeFile('./src/translation.schema.d.ts', schema);
