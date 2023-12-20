9b972d0c1e3ab47f5d603dd177d14cf9c513e228
Alexey Iskhakov <a.raiondesu@gmail.com>
2023-12-20 21:11:14 +0400

    Update build

--- packages/plugins/arrays/dist/index.cjs
+++ packages/plugins/arrays/dist/index.cjs
@@ -28,18 +28,18 @@ function match(value) {
   return Array.isArray(value);
 }
 var ArraysPlugin = (defaultSeparator = " ") => (0, import_plugins.createPlugin)("ArraysPlugin", match, {
-  translate(referenceParams, separator = defaultSeparator) {
+  translate(referenceParamsByKey, separator = defaultSeparator) {
     const startsWithIndex = /^.*?:/;
     const processReference = (referencedKey) => {
       if (startsWithIndex.test(referencedKey)) {
         const [argIndexName, inputKey] = referencedKey.split(":");
         const argIndex = isNaN(Number(argIndexName)) ? 0 : Number(argIndexName);
-        const references = normalizeRefs(referenceParams, inputKey);
+        const references = normalizeRefs(referenceParamsByKey, inputKey);
         return [String(references[argIndex])];
       }
-      const result2 = referencedKey in referenceParams ? this.translate(
+      const result2 = referencedKey in referenceParamsByKey ? this.translate(
         referencedKey,
-        ...normalizeRefs(referenceParams, referencedKey)
+        ...normalizeRefs(referenceParamsByKey, referencedKey)
       ) : this.translate(referencedKey);
       if (typeof result2 === "string") {
         return [result2];
@@ -50,18 +50,18 @@ var ArraysPlugin = (defaultSeparator = " ") => (0, import_plugins.createPlugin)(
       if (typeof refK === "string") {
         return [...arr, ...processReference(refK)];
       }
-      const refParamK = Object.keys(refK)[0];
-      if (typeof referenceParams[refParamK] === "undefined" && refParamK in refK) {
-        referenceParams[refParamK] = normalizeRefs(refK, refParamK);
-      } else if (refParamK in refK) {
-        const references = normalizeRefs(referenceParams, refParamK);
-        const inlineParams = normalizeRefs(refK, refParamK);
-        referenceParams[refParamK] = references.map((param, i) => {
+      const refParamKey = Object.keys(refK)[0];
+      if (typeof referenceParamsByKey[refParamKey] === "undefined" && refParamKey in refK) {
+        referenceParamsByKey[refParamKey] = normalizeRefs(refK, refParamKey);
+      } else if (refParamKey in refK) {
+        const referenceParams = normalizeRefs(referenceParamsByKey, refParamKey);
+        const inlineParams = normalizeRefs(refK, refParamKey);
+        referenceParamsByKey[refParamKey] = referenceParams.map((param, i) => {
           const inlineParam = inlineParams[i];
           return typeof param === "object" && typeof inlineParam === "object" ? { ...inlineParam, ...param } : param ?? inlineParam;
         });
       }
-      return [...arr, ...processReference(refParamK)];
+      return [...arr, ...processReference(refParamKey)];
     }, []);
     if (typeof separator === "string") {
       return result.join(separator);
